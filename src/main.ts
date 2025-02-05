5089a8863f89b711d3ddcb169e7dc4666605d635
divyanshdangi11 <99112217+divyanshdangi11@users.noreply.github.com>
2025-02-06 00:22:31 +0530

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,11 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(4000);
+
+  // Use the PORT environment variable or fallback to 3000
+  const port = process.env.PORT || 3000;
+
+  await app.listen(port);
+  console.log(`Application is running on: http://localhost:${port}`);
 }
 bootstrap();
